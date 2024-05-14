acc9acf01d294cc6817afd87b0697b0ea7d87ae8
sachin <sachin.hm@smartbear.com>
2024-05-14 18:10:42 +0530

    refactored the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,28 +3,16 @@ package org.example;
 public class Main {
 
     public static Employee employee;
-    public static Company company;
-    public static Util util = new Util();
-    public static CompanyUtil companyUtil = new CompanyUtil();
 
     public static void main(String[] args) {
         System.out.println("creating Employee and Company");
         createEmployee();
-        createCompany();
         System.out.println("updating Employee");
-        updateEmployee();
     }
 
     private static void createEmployee() {
-        employee = util.createEmployee("John", 30, 5000, "IT");
+        employee = new Employee("John", 30, 50000, "IT");
         System.out.println(employee.getName() + " " + employee.getAge() + " " + employee.getSalary() + " " + employee.getDepartment());
     }
 
-    private static void updateEmployee() {
-        util.updateEmployee(employee, 31, 55000, "HR");
-    }
-
-    private static void createCompany() {
-        company = companyUtil.createCompany("ABC", "IT", "India", "Bangalore");
-    }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,4 @@
 
-import org.example.Company;
-import org.example.CompanyUtil;
 import org.example.Employee;
 import org.example.Main;
 import org.example.Util;
@@ -12,20 +10,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     private Util utilMock;
-    private CompanyUtil companyUtilMock;
 
     @BeforeEach
     void setUp() {
         utilMock = new Util();
-        companyUtilMock = new CompanyUtil();
-        Main.util = utilMock;
-        Main.companyUtil = companyUtilMock;
     }
 
     @Test
     void main() {
         Employee mockEmployee = new Employee("John", 30, 5000, "IT");
-        Company mockCompany = new Company("ABC", "IT", "India", "Bangalore");
         Main.main(new String[]{});
 
     }
